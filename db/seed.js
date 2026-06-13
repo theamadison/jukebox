ecbcbd110a68b9049ef2fe5a1c8871b65a1c9075
theamadison <alfoxthea@gmail.com>
2026-06-13 13:42:44 -0400

    Add files via upload

--- db/seed.js
+++ db/seed.js
@@ -6,5 +6,12 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seed() {
-  // TODO
+  for (let i = 1; i <= 20; i++) {
+    await createPlaylist("Playlist " + i, "lorem ipsum playlist description");
+    await createTrack("Track " + i, i * 50000);
+  }
+  for (let i = 1; i <= 15; i++) {
+    const playlistId = 1 + Math.floor(i / 2);
+    await createPlaylistTrack(playlistId, i);
+  }
 }
